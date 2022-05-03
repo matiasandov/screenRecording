7ccb6085b92a8c4d7707ce2902bbc7ca69118b27
Matt <a01422885@itesm.mx>
2022-05-03 12:04:49 -0500

    version sin separar componentes boton y sin estado en clase

---
+++ src/components/bar.jsx
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import "./screenRecorder.css";
+
+class ScreenRecorderHome extends Component {
+
+
+     handlePause(rm) {
+        return rm.pause();
+    };
+
+    handleStart = async () => {
+        // stream is a MediaStream object.
+        const stream = await navigator.mediaDevices.getDisplayMedia({
+            video: {
+                mediaSource: "screen",
+            },
+        });
+        // data = Blob array;
+        const data = [];
+        
+        const mediaRecorder = new MediaRecorder(stream);
+    
+        // continous contruction of the blob array;
+        mediaRecorder.ondataavailable = (e) => {
+            data.push(e.data);
+        };
+    
+        
+        mediaRecorder.start(); // we need to start the recording.
+    
+        console.log(mediaRecorder.state);
+    
+        
+    
+        // event listener to stop recording //
+        var btn3 = document.getElementById("button3");
+        btn3.addEventListener("click", () => {
+            // the stream is finished.
+            stream.getTracks().forEach( track => track.stop() ); // stop all tracks.
+            //stream.active = false; this property is read only, cannot be directly affected.
+            console.log(stream.active);
+        })
+    
+    
+        // event listener to pause and resume recording //
+        var btn4 = document.getElementById("button4");
+        btn4.addEventListener("click", () => {
+    
+            if (mediaRecorder.state === "recording") {
+                //stream.getTracks().forEach( track => track.pause() ); // pause all tracks.
+                mediaRecorder.pause();
+                console.log("video paused");
+                console.log(mediaRecorder.state);
+            }
+            else if (mediaRecorder.state === "paused") {
+                // stream.getTracks().forEach( track => track.resume() ); // resume all tracks.
+                mediaRecorder.resume();
+                console.log("video resumed");
+                console.log(mediaRecorder.state);
+            }
+        })
+    
+        //mediaRecorder.stop(); A sole stop doesn't stop all tracks.
+        
+        mediaRecorder.onstop = (e) => {
+            console.log(stream.active);
+            console.log(mediaRecorder.state);
+            // When stopped display video.
+            document.querySelector("video").src = URL.createObjectURL(
+                new Blob(data, {
+                    type: data[0].type,
+                })
+            );
+        };
+    };
+
+//1. estandares de codificacion
+//2. El objeto media recorder deberia ser parte del estado probablemente
+//3. separar botones como list group
+
+    render() { 
+        
+        return (
+            <div>
+
+            <h1>HOLA </h1>
+            <div>
+              <video id='myVideo' width="900" height="500" controls></video>
+            </div>
+             
+      
+             
+             <div id="buttons">
+                 <button className="btn" id="button2" onClick={this.start}>Click to begin!</button> 
+                 <button className="btn" id="button3">Click to stop!</button>
+                 <button className="btn" id="button4">Click to pause/resume!</button>
+             </div>
+      
+          </div>
+        );
+    }
+}
+ 
+export default ScreenRecorderHome;
